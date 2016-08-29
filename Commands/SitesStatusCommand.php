24dfc153f38da7e427b222f9edb7773801fb222e
Ed Reel <ed.reel@drupalconnect.com>
2016-08-29 10:30:06 -0500

    Removed private filtering functions and used public functions instead

--- Commands/SitesStatusCommand.php
+++ Commands/SitesStatusCommand.php
@@ -59,37 +59,31 @@ class SitesStatusCommand extends TerminusCommand {
    * @alias st
    */
   public function status($args, $assoc_args) {
-    // Always fetch a fresh list of sites.
-    if (!isset($assoc_args['cached'])) {
-      $this->sites->rebuildCache();
-    }
-    $sites = $this->sites->all();
-
-    if (isset($assoc_args['team'])) {
-      $sites = $this->filterByTeamMembership($sites);
-    }
-    if (isset($assoc_args['org'])) {
-      $org_id = $this->input()->orgId(
+    $options = [
+      'org_id'    => $this->input()->optional(
         [
-          'allow_none' => true,
-          'args'       => $assoc_args,
-          'default'    => 'all',
+          'choices' => $assoc_args,
+          'default' => null,
+          'key'     => 'org',
         ]
-      );
-      $sites = $this->filterByOrganizationalMembership($sites, $org_id);
-    }
+      ),
+      'team_only' => isset($assoc_args['team']),
+    ];
+    $this->sites->fetch($options);
 
     if (isset($assoc_args['name'])) {
-      $sites = $this->filterByName($sites, $assoc_args['name']);
+      $this->sites->filterByName($assoc_args['name']);
     }
 
     if (isset($assoc_args['owner'])) {
       $owner_uuid = $assoc_args['owner'];
       if ($owner_uuid == 'me') {
-        $owner_uuid = Session::getData()->user_uuid;
+        $owner_uuid = $this->user->id;
       }
-      $sites = $this->filterByOwner($sites, $owner_uuid);
+      $this->sites->filterByOwner($owner_uuid);
     }
+
+    $sites = $this->sites->all();
 
     if (count($sites) == 0) {
       $this->log()->warning('You have no sites.');
@@ -182,90 +176,6 @@ class SitesStatusCommand extends TerminusCommand {
     $this->output()->outputRecordList($site_rows, $site_labels);
     $this->output()->outputRecordList($env_rows, $env_labels);
 
-  }
-
-  /**
-   * Filters an array of sites by whether the user is an organizational member
-   *
-   * @param Site[] $sites An array of sites to filter by
-   * @param string $regex Non-delimited PHP regex to filter site names by
-   * @return Site[]
-   */
-  private function filterByName($sites, $regex = '(.*)') {
-    $filtered_sites = array_filter(
-      $sites,
-      function($site) use ($regex) {
-        preg_match("~$regex~", $site->get('name'), $matches);
-        $is_match = !empty($matches);
-        return $is_match;
-      }
-    );
-    return $filtered_sites;
-  }
-
-  /**
-   * Filters an array of sites by whether the user is an organizational member
-   *
-   * @param Site[] $sites      An array of sites to filter by
-   * @param string $owner_uuid UUID of the owning user to filter by
-   * @return Site[]
-   */
-  private function filterByOwner($sites, $owner_uuid) {
-    $filtered_sites = array_filter(
-      $sites,
-      function($site) use ($owner_uuid) {
-        $is_owner = ($site->get('owner') == $owner_uuid);
-        return $is_owner;
-      }
-    );
-    return $filtered_sites;
-  }
-
-  /**
-   * Filters an array of sites by whether the user is an organizational member
-   *
-   * @param Site[] $sites  An array of sites to filter by
-   * @param string $org_id ID of the organization to filter for
-   * @return Site[]
-   */
-  private function filterByOrganizationalMembership($sites, $org_id = 'all') {
-    $filtered_sites = array_filter(
-      $sites,
-      function($site) use ($org_id) {
-        $memberships    = $site->get('memberships');
-        foreach ($memberships as $membership) {
-          if ((($org_id == 'all') && ($membership['type'] == 'organization'))
-            || ($membership['id'] === $org_id)
-          ) {
-            return true;
-          }
-        }
-        return false;
-      }
-    );
-    return $filtered_sites;
-  }
-
-  /**
-   * Filters an array of sites by whether the user is a team member
-   *
-   * @param Site[] $sites An array of sites to filter by
-   * @return Site[]
-   */
-  private function filterByTeamMembership($sites) {
-    $filtered_sites = array_filter(
-      $sites,
-      function($site) {
-        $memberships    = $site->get('memberships');
-        foreach ($memberships as $membership) {
-          if ($membership['name'] == 'Team') {
-            return true;
-          }
-        }
-        return false;
-      }
-    );
-    return $filtered_sites;
   }
 
   /**
